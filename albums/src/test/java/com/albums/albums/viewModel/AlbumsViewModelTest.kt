6a1c40746d65af19afb72bdf4224d0c8acb9092b
ChenYY <Chen.yingying.01988@gmail.com>
2021-09-10 14:42:29 +0100

    added test coroutine rule and test passed sucessfully call

---
+++ albums/src/test/java/com/albums/albums/viewModel/AlbumsViewModelTest.kt
@@ -0,0 +1,58 @@
+package com.albums.albums.viewModel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.albums.albums.data.model.AlbumItem
+import com.albums.albums.repository.AlbumsRepository
+import com.albums.albums.util.TestCoroutineRule
+import com.albums.core.network.Resource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class AlbumsViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @Mock
+    private lateinit var albumsRepository: AlbumsRepository
+
+    @Mock
+    private lateinit var apiAlbumsObserver: Observer<Resource<List<AlbumItem>>>
+
+    private lateinit var viewModel: AlbumsViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = AlbumsViewModel(albumsRepository)
+    }
+
+    @Test
+    fun `given server response 200 when fetch should return success`() {
+        startFetchingAlbumsResult(Resource.success(data = emptyList()))
+    }
+
+    private fun startFetchingAlbumsResult(data: Resource<List<AlbumItem>>) {
+        testCoroutineRule.runBlockingTest {
+            Mockito.doReturn(data).`when`(albumsRepository).getAlbums()
+            viewModel.fetchAllAlbums()
+            viewModel.getAlbums().observeForever(apiAlbumsObserver)
+
+            Mockito.verify(albumsRepository).getAlbums()
+            Mockito.verify(apiAlbumsObserver).onChanged(data)
+            viewModel.getAlbums().removeObserver(apiAlbumsObserver)
+        }
+    }
+}
